56488c6f1834c44494ddfc7a1cfbc61f3007f8c0
AndreiLegram <andrei.legramante@gmail.com>
2024-09-30 21:58:31 -0300

    update 30/09

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,5 +12,5 @@ document.body.onload = () => {
     const n1 = Number(prompt("Entre com o primeiro número:"))
     const n2 = parseInt(prompt("Entre com o segundo número:"))
     const n3 = +prompt("Entre com o terceiro número:")
-    alert(soma(n1 + n2 + n3))
+    alert(soma(n1, n2, n3))
 }
